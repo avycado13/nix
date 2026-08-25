1049fc856b21697b6acbf18aa0a319221de39657
avycado13 <108358183+avycado13@users.noreply.github.com>
2026-08-25 16:30:36 -0700

    remove some unnessacry stuff

--- toml.nix
+++
@@ -1,8 +0,0 @@
-let
-  # Read the TOML file content as a string
-  tomlString = builtins.readFile ./dns.toml;
-
-  # Parse the TOML string into a Nix attribute set
-  config = builtins.fromTOML tomlString;
-in
-config
